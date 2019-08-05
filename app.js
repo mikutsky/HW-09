2ae9458f49180454bde4df07210a7d01cbd95e7a
Dmitry Mikutsky <dmitry.mikutsky@gmail.com>
2019-08-06 01:35:18 +0300

    All comments taken into account.

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
 //Модуль http предачи и получения сообщений
-const http = (function() {
-  return {
-    get(url, cb) {
-      try {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", url);
-        xhr.addEventListener("load", () => {
-          if (Math.floor(xhr.status / 100) !== 2) {
-            cb(`Error. Status code: ${xhr.status}`, xhr);
-            return;
-          }
-          const response = JSON.parse(xhr.responseText);
-          cb(null, response);
-        });
+const http = {
+  get(url, cb) {
+    try {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", url);
 
-        xhr.addEventListener("error", () => {
+      xhr.addEventListener("load", () => {
+        if (xhr.status < 200 && xhr.status > 299) {
           cb(`Error. Status code: ${xhr.status}`, xhr);
-        });
-
-        xhr.send();
-      } catch (error) {
-        cb(error);
-      }
-    },
-    post(url, body, headers, cb) {
-      try {
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", url);
-        xhr.addEventListener("load", () => {
-          if (Math.floor(xhr.status / 100) !== 2) {
-            cb(`Error. Status code: ${xhr.status}`, xhr);
-            return;
-          }
-          const response = JSON.parse(xhr.responseText);
-          cb(null, response);
-        });
-
-        xhr.addEventListener("error", () => {
-          cb(`Error. Status code: ${xhr.status}`, xhr);
-        });
-
-        if (headers) {
-          Object.entries(headers).forEach(([key, value]) => {
-            xhr.setRequestHeader(key, value);
-          });
+          return;
         }
+        const response = JSON.parse(xhr.responseText);
+        cb(null, response);
+      });
 
-        xhr.send(JSON.stringify(body));
-      } catch (error) {
-        cb(error);
-      }
+      xhr.addEventListener("error", () => {
+        cb(`Error. Status code: ${xhr.status}`, xhr);
+      });
+
+      xhr.send();
+    } catch (error) {
+      cb(error);
     }
-  };
-})();
+  },
+  post(url, body, headers, cb) {
+    try {
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", url);
+
+      xhr.addEventListener("load", () => {
+        if (xhr.status < 200 && xhr.status > 299) {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+          return;
+        }
+        const response = JSON.parse(xhr.responseText);
+        cb(null, response);
+      });
+
+      xhr.addEventListener("error", () => {
+        cb(`Error. Status code: ${xhr.status}`, xhr);
+      });
+
+      if (headers) {
+        Object.entries(headers).forEach(([key, value]) => {
+          xhr.setRequestHeader(key, value);
+        });
+      }
+
+      xhr.send(JSON.stringify(body));
+    } catch (error) {
+      cb(error);
+    }
+  }
+};
 
 //Модуль для работы с новостным API
 const newsService = (function() {
